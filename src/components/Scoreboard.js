e86efffd9d3efbd69dab1e16e731f501fd78a48f
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-07-11 11:52:39 -0400

    scoreboard component initial commit

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Scoreboard = (props) => {
+  const devStyle = {
+    border: "1px solid goldenrod",
+  };
+
+  const { score, bestScore } = props;
+
+  return (
+    <div className="scoreboard" style={devStyle}>
+      <h3>Scoreboard</h3>
+      <p id="score">Score: {score}</p>
+      <p id="best-score">Best Score: {bestScore}</p>
+    </div>
+  );
+};
+
+export default Scoreboard;
