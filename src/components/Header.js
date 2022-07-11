786ecdc29403e458a341fd54837baeb868ba96fa
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-07-11 11:52:24 -0400

    header component initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  const devStyle = { border: "1px solid blue" };
+
+  return (
+    <div className="Header" style={devStyle}>
+      <h1>OSRS Memory Game!</h1>
+    </div>
+  );
+};
+
+export default Header;
